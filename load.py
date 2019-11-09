e6484975eb2217009db88fdd463da2085dfc9490
Gabriel Haggstrom <gshaggstrom@gmail.com>
2019-11-09 16:35:46 -0300

    Implementa load_dataset; Adapta arquivos de dataset para o formato lido por load_dataset.

--- load.py
+++ load.py
@@ -1,42 +1,22 @@
 import json
-import pandas
+import numpy as np
 
-def load_dataset(name):
-  """ name: Nome do dataset, p.ex. 'credit-g'
-  """
+def load_dataset(filename):
+  dataset = []
+  with open(filename, 'r') as f:
+    line = f.readline()
+    while line:
+      [input_string, output_string] = line.split(';')
+      input_vector = np.array(np.mat(input_string).T)
+      output_vector = np.array(np.mat(output_string).T)
+      dataset.append({
+        'input': input_vector,
+        'output': output_vector
+      })
+      line = f.readline()
+    
+  return dataset
 
-  # Lê os metadados
-  with open('metadata/{}.json'.format(name), 'r') as f:
-      metadata = json.load(f)
-  
-  # Lê os dados
-  data = pandas.read_csv('datasets/{}.csv'.format(name))
-
-  # Cria um dicionário de atributos, e descobre o atributo alvo
-  target = ""
-  attributes = {}
-  for feature in metadata['features']:
-    attribute = {}
-    attribute['type'] = feature['type']
-    if 'distr' in feature:
-      attribute['values'] = feature['distr'][0]
-    if 'target' in feature and feature['target'] == "1":
-      target = feature['name']
-    attributes[feature['name']] = attribute
-
-  # Um dataset é um dicionário com dados e informações dos atributos.
-  # 'data' é um pandas DataFrame.
-  # 'attributes' é uma lista de dicionários. Cada dicionário contém uma série de informações de um atributo. As mais
-  # importantes são:
-  #   ['name']: Nome do atributo.
-  #   ['type']: Tipo do atributo ('nominal' ou 'numeric').
-  #   ['values']: Presente somente se o atributo for nominal. Lista de valores possíveis do atributo.
-  # 'target' é uma string, o nome do atributo alvo.
-  return {
-    'data': data,
-    'attributes': attributes,
-    'target': target
-  }
 
 def load_network_structure(filename):
   # TODO
@@ -44,6 +24,7 @@ def load_network_structure(filename):
     'regularization': 0.0,
     'layer_sizes': [1, 2, 1]
   }
+
 
 def load_weights(filename):
   # TODO: Retornar matrizes de pesos para vetorização.
